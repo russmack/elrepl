3f0819f4ee2a18d3ed3f28d62d65595707a332de
russmack <russ_mack@hotmail.com>
2014-08-05 01:55:19 +0100

    Changed to show version of es, not repl.

--- handleversion.go
+++ handleversion.go
@@ -1,13 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 func init() {
 	h := NewHandler()
 	h.CommandName = "version"
 	h.CommandPattern = "(version)(( )(.*))"
 	h.HandlerFunc = func(cmd *Command) string {
-		return `
-	elRepl version 0.1
-	`
+		url := fmt.Sprintf("http://%s:%s", server.host, server.port)
+		fmt.Println("Request:", url)
+		res, err := getHttpResource(url)
+		if err != nil {
+			return err.Error()
+		}
+		return res
 	}
 	HandlerRegistry[h.CommandName] = h
 }
