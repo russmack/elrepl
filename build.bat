68b1c60460d9537bcf109a05dd84276f2783b7df
russmack <russ_mack@hotmail.com>
2014-08-05 01:57:20 +0100

    Added windows build file.

---
+++ build.bat
@@ -0,0 +1,2 @@
+go build elrepl.go command.go commandparser.go dispatcher.go httpclient.go handler.go handlealias.go handledir.go handleexit.go handleget.go handlehelp.go handlehost.go handleindex.go handleload.go handlelog.go handleport.go handlepost.go handleput.go handlereindex.go handlerun.go handleversion.go handleduplicatescount.go
+pause
