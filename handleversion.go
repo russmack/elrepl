057a7645da904d407a238269873d298c89cd3f26
russmack <russ_mack@hotmail.com>
2014-08-06 18:23:27 +0100

    Separated parsing from http call.

--- handleversion.go
+++ handleversion.go
@@ -9,10 +9,18 @@ func init() {
 	h := NewHandler()
 	h.CommandName = "version"
 	h.CommandPattern = "(version)(( )(.*))"
+	h.CommandParser = func(cmd *Command) map[string]string {
+		m := make(map[string]string)
+		m["scheme"] = "http"
+		m["host"] = server.host
+		m["port"] = server.port
+		return m
+	}
 	h.HandlerFunc = func(cmd *Command) string {
+		m := h.CommandParser(cmd)
 		u := new(url.URL)
-		u.Scheme = "http"
-		u.Host = server.host + ":" + server.port
+		u.Scheme = m["scheme"]
+		u.Host = m["host"] + ":" + m["port"]
 		fmt.Println("Request:", u)
 		res, err := getHttpResource(u.String())
 		if err != nil {
