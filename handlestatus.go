44e06a4bf86126c38e2e3905da5c2105b2ac150b
russmack <russ_mack@hotmail.com>
2014-08-06 18:22:44 +0100

    Separated parsing from http call.

--- handlestatus.go
+++ handlestatus.go
@@ -10,48 +10,35 @@ func init() {
 	h := NewHandler()
 	h.CommandName = "status"
 	h.CommandPattern = "(status)( )(.*)"
-	h.HandlerFunc = func(cmd *Command) string {
+	h.CommandParser = func(cmd *Command) map[string]string {
 		argParts := strings.Split(cmd.Args, " ")
-		//arg := "_status?pretty"
-		index := ""
-		//urlString := ""
-		res := ""
-		if len(argParts) == 0 {
-			u := new(url.URL)
-			u.Scheme = "http"
-			u.Host = server.host + ":" + server.port
-			u.Path = "_status"
-			q := u.Query()
-			q.Add("pretty", "true")
-			u.RawQuery = q.Encode()
-			fmt.Println("Request:", u)
-			getRes, err := getHttpResource(u.String())
-			if err != nil {
-				return err.Error()
-			}
-			res = getRes
-		} else if len(argParts) == 1 {
-			if argParts[0] == "index" { // Override session var.
-				index = server.index
-			} else {
-				index = argParts[0]
-			}
-
-			u := new(url.URL)
-			u.Scheme = "http"
-			u.Host = server.host + ":" + server.port
-			u.Path = index + "/" + "_status"
-			q := u.Query()
-			q.Add("pretty", "true")
-			u.RawQuery = q.Encode()
-
-			//urlString = fmt.Sprintf("http://%s:%s/%s/%s", server.host, server.port, index, arg)
-			fmt.Println("Request:", u)
-			getRes, err := getHttpResource(u.String())
-			if err != nil {
-				return err.Error()
-			}
-			res = getRes
+		m := make(map[string]string)
+		m["scheme"] = "http"
+		m["host"] = server.host
+		m["port"] = server.port
+		m["endpoint"] = "_status"
+		if argParts[0] != "/" {
+			m["index"] = argParts[0]
+		}
+		return m
+	}
+	h.HandlerFunc = func(cmd *Command) string {
+		m := h.CommandParser(cmd)
+		u := new(url.URL)
+		u.Scheme = m["scheme"]
+		u.Host = m["host"] + ":" + m["port"]
+		index, ok := m["index"]
+		if ok {
+			index += "/"
+		}
+		u.Path = index + m["endpoint"]
+		q := u.Query()
+		q.Add("pretty", "true")
+		u.RawQuery = q.Encode()
+		fmt.Println("Request:", u)
+		res, err := getHttpResource(u.String())
+		if err != nil {
+			return err.Error()
 		}
 		return res
 	}
