5b32ed78c6fb6d67e7621c5d5a99362c9cc9a8a8
russmack <russ_mack@hotmail.com>
2014-08-08 02:22:58 +0100

    Added help and usage.

--- handleindex.go
+++ handleindex.go
@@ -1,16 +1,40 @@
 package main
 
+import (
+	"strings"
+)
+
 func init() {
 	h := NewHandler()
 	h.CommandName = "index"
 	h.CommandPattern = "(index)(( )(.*))"
+	h.CommandParser = func(cmd *Command) (map[string]string, bool) {
+		if cmd.Args == "/?" {
+			return nil, false
+		}
+		argParts := strings.Split(cmd.Args, " ")
+		if len(argParts) > 1 {
+			return nil, false
+		}
+		if len(argParts) == 1 && argParts[0] == "" {
+			return nil, true
+		} else {
+			m := make(map[string]string)
+			m["index"] = argParts[0]
+			return m, true
+		}
+	}
 	h.HandlerFunc = func(cmd *Command) string {
-		if cmd.Args == "" {
+		m, ok := h.CommandParser(cmd)
+		if !ok {
+			return "Usage: index [indexName]"
+		}
+		indexName, ok := m["index"]
+		if !ok {
 			return "Index: " + server.index
 		} else {
-			arg := cmd.Args
-			server.index = arg
-			return "Set index: " + arg
+			server.index = indexName
+			return "Set index: " + indexName
 		}
 	}
 	HandlerRegistry[h.CommandName] = h
