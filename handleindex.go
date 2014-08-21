c1e65ee1d1b78182ea13f50a14efaf01bc4ed8d7
russmack <russ_mack@hotmail.com>
2014-08-21 18:18:09 +0100

    Added create index.

--- handleindex.go
+++ handleindex.go
@@ -1,20 +1,35 @@
 package main
 
-import ()
+import (
+	"errors"
+	"fmt"
+	"net/url"
+)
 
 func init() {
 	h := NewHandler()
 	h.CommandName = "index"
 	h.CommandPattern = "(index)(( )(.*))"
-	h.Usage = "index [indexName]"
+	h.Usage = "index [ indexName | (create host port indexName) ]"
 	h.CommandParser = func(cmd *Command) (ParseMap, bool) {
 		p := ParseMap{}
+		p["scheme"] = "http"
 
 		switch cmd.Args[0] {
 		case "/?":
 			return p, false
 		case "":
 			if len(cmd.Args) == 1 { // get index
+				return p, true
+			} else {
+				return p, false
+			}
+		case "create":
+			if len(cmd.Args) == 4 { // set index
+				p["action"] = cmd.Args[0]
+				p["host"] = cmd.Args[1]
+				p["port"] = cmd.Args[2]
+				p["index"] = cmd.Args[3]
 				return p, true
 			} else {
 				return p, false
@@ -33,13 +48,34 @@ func init() {
 		if !ok {
 			return usageMessage(h.Usage)
 		}
-		indexName, ok := p["index"]
-		if !ok {
+		action, actionOk := p["action"]
+		indexName, indexOk := p["index"]
+		if !indexOk {
 			return "Index: " + server.index
-		} else {
+		}
+		if !actionOk {
 			server.index = indexName
 			return "Set index: " + indexName
 		}
+		if action == "create" {
+			// Create new index
+			//curl -XPUT "http://10.1.1.12:9200/podcasts-2014-07-23-1930/"
+			u := new(url.URL)
+			u.Scheme = p["scheme"]
+			u.Host = p["host"] + ":" + p["port"]
+			u.Path = indexName
+			q := u.Query()
+			q.Add("pretty", "true")
+			u.RawQuery = q.Encode()
+			fmt.Println("Request:", u)
+			err := errors.New("")
+			res, err := putHttpResource(u.String(), "")
+			if err != nil {
+				return err.Error()
+			}
+			return res
+		}
+		return usageMessage(h.Usage)
 	}
 	HandlerRegistry[h.CommandName] = h
 }
