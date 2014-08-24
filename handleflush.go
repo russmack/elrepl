7060d9e69f688bada71fbaadd368788f0fb1436f
russmack <russ_mack@hotmail.com>
2014-08-24 23:54:05 +0100

    Added flush command.

---
+++ handleflush.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func init() {
+	h := NewHandler()
+	h.CommandName = "flush"
+	h.CommandPattern = "(flush)( )(.*)"
+	h.Usage = "flush [/|indexName]"
+	h.CommandParser = func(cmd *Command) (ParseMap, bool) {
+		p := ParseMap{}
+		p["scheme"] = "http"
+		p["host"] = server.host
+		p["port"] = server.port
+		p["endpoint"] = "_flush"
+
+		switch cmd.Args[0] {
+		case "/?":
+			return p, false
+		case "":
+			if len(cmd.Args) == 1 {
+				return p, true
+			} else {
+				return p, false
+			}
+		default:
+			if len(cmd.Args) == 1 {
+				p["index"] = cmd.Args[0]
+				return p, true
+			} else {
+				return p, false
+			}
+		}
+	}
+	h.HandlerFunc = func(cmd *Command) string {
+		p, ok := h.CommandParser(cmd)
+		if !ok {
+			return usageMessage(h.Usage)
+		}
+		u := new(url.URL)
+		u.Scheme = p["scheme"]
+		u.Host = p["host"] + ":" + p["port"]
+		index, ok := p["index"]
+		if ok {
+			index += "/"
+		}
+		u.Path = index + p["endpoint"]
+		q := u.Query()
+		q.Add("pretty", "true")
+		u.RawQuery = q.Encode()
+		fmt.Println("Request:", u)
+		res, err := postHttpResource(u.String(), "")
+		if err != nil {
+			return err.Error()
+		}
+		return res
+	}
+	HandlerRegistry[h.CommandName] = h
+}
