fb18390996ad849dc5a8bd652c832952ec4f3bb7
russmack <russ_mack@hotmail.com>
2014-09-01 02:50:12 +0100

    Now requires host and port.

--- handlepost.go
+++ handlepost.go
@@ -7,33 +7,16 @@ import (
 // curl -XPOST "http://localhost:9200/movies/_search?pretty" -d'{ ... body ... }''
 // becomes
 // post _search?pretty { "query": { "term": { "director": "scott" } } }
-// Currently, must be on single line.
+// TODO: Currently, must be on single line.
 func init() {
 	h := NewHandler()
 	h.CommandName = "post"
 	h.CommandPattern = "((?i)post(?-i))( )(.*)"
 	h.HandlerFunc = func(cmd *Command) string {
-		queryHost := server.host
-		queryPort := server.port
-
-		//fmt.Println("cmd:", cmd.Name)
-		//arg := cmd.Args[0]
-		//fmt.Println("arg:", arg)
-		//fmt.Println("arg1:", cmd.Args[1])
-		//fmt.Println("args:", cmd.Args)
-		//bodyIdx := strings.Index(arg, "{")
-		//fmt.Println("bodyidx:", bodyIdx)
-
-		//queryArgs := arg[:bodyIdx]
-		//queryArgs = strings.TrimPrefix(queryArgs, "/")
-		//queryArgs = strings.TrimSpace(queryArgs)
-		//body := arg[bodyIdx:]
-		//queryArgs := cmd.Args[0]
-		//body := cmd.Args[1]
-		queryArgs := cmd.Tokens[1]
-
+		queryHost := cmd.Tokens[1]
+		queryPort := cmd.Tokens[2]
+		queryArgs := cmd.Tokens[3]
 		url := fmt.Sprintf("http://%s:%s/%s", queryHost, queryPort, queryArgs)
-
 		fmt.Println("Request:", url)
 		res, err := postHttpResource(url, cmd.Body)
 		if err != nil {
