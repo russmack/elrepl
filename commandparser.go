006a3bf30db55d98dba4acc887f70c56b3c0fb62
russmack <russ_mack@hotmail.com>
2014-09-01 15:59:12 +0100

    Cleaned.

--- commandparser.go
+++ commandparser.go
@@ -15,7 +15,6 @@ func NewCommandParser() *CommandParser {
 
 func (p *CommandParser) Parse(entry string) (*Command, error) {
 	bodyStart := strings.Index(entry, "{")
-	//parts := strings.SplitN(entry, "{", 2)
 	instr := ""
 	body := ""
 	if bodyStart == -1 {
@@ -26,16 +25,7 @@ func (p *CommandParser) Parse(entry string) (*Command, error) {
 	if bodyStart > -1 {
 		body = entry[bodyStart:]
 	}
-	//if len(parts) > 0 {
-	//instr = parts[0]
-	//if len(parts) > 1 {
-	//	body = parts[1]
-	//}
 	cmdTokens := strings.Split(instr, " ")
 	cmdName := cmdTokens[0]
-	//cmdArgs := tokens[1:]
-	//if len(cmdArgs) == 0 {
-	//	cmdArgs = append(cmdArgs, "")
-	//}
 	return NewCommand(instr, body, cmdName, cmdTokens), nil
 }
