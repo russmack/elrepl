ed8e18c2f97bf16a196938b0414b9bb9d120a7ba
russmack <russ_mack@hotmail.com>
2014-09-01 01:37:24 +0100

    Updated to exclude host and port.

--- crosscompile.bat
+++ crosscompile.bat
@@ -2,6 +2,6 @@ setlocal
 set GOOS=linux
 set GOARCH=amd64
 set CGO_ENABLED=0
-go build -o elrepl.bin elrepl.go command.go commandparser.go dispatcher.go httpclient.go handler.go handlealias.go handleclose.go handlecount.go handledir.go handledoc.go handleduplicatescount.go handleenv.go handleexit.go handleflush.go handleget.go handlehelp.go handlehost.go handleindex.go handleload.go handlelog.go handlemapping.go handleopen.go handleoptimize.go handleport.go handlepost.go handleput.go handlerecovery.go handlerefresh.go handlereindex.go handlerun.go handlesegments.go handlesettings.go handlestats.go handlestatus.go handleversion.go
+go build -o elrepl.bin elrepl.go command.go commandparser.go dispatcher.go httpclient.go handler.go handlealias.go handleclose.go handlecount.go handledir.go handledoc.go handleduplicatescount.go handleenv.go handleexit.go handleflush.go handleget.go handlehelp.go handleindex.go handleload.go handlelog.go handlemapping.go handleopen.go handleoptimize.go handlepost.go handleput.go handlerecovery.go handlerefresh.go handlereindex.go handlerun.go handlesegments.go handlesettings.go handlestats.go handlestatus.go handleversion.go
 endlocal
 pause
